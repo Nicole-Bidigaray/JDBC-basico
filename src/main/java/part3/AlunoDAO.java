89e8741450a74a09973f5de5afba438eaaea3181
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2021-08-13 12:05:17 -0300

    Add all files Connection Java with JDBC

--- src/main/java/part3/AlunoDAO.java
+++ src/main/java/part3/AlunoDAO.java
@@ -12,42 +12,70 @@ public class AlunoDAO {
     // 1 - Consulta
     public List<Aluno> list() {
         //Preparar lista que irá retornar alunos após consultar o banco de dados;
+
         List<Aluno> alunos = new ArrayList<>();
 
         try (Connection conn = ConnectionFactory.getConnection()) {
-            //Preparar consulta SQL.
-            String sql = "SELECT * FROM aluno";
 
-            //Preparar statement com os parâmetros recebidos (nesta função não tem parâmetros, pois irá retornar todos os valores da tabela aluno)
-            PreparedStatement stmt = conn.prepareStatement(sql);
+            PreparedStatement prst = conn.prepareStatement("SELECT * FROM aluno");
 
-            //Executa consulta e armazena o retorno da consulta no objeto "rs".
-            ResultSet rs = stmt.executeQuery();
+            ResultSet rs = prst.executeQuery();
 
-            //Criar um objeto aluno e guardar na lista de alunos.
-            while(rs.next()){
-                int id = rs.getInt("id");
-                String nome = rs.getString("nome");
-                int idade = rs.getInt("idade");
-                String estado = rs.getString("estado");
+            while (rs.next()) {
 
-                alunos.add(new Aluno(
-                        id,
-                        nome,
-                        idade,
-                        estado
-                ));
+//                Aluno(String nome, int idade, String estado)
+                Aluno aluno = new Aluno(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getInt("idade"),
+                        rs.getString("estado")
+                );
+
+                alunos.add(aluno);
             }
-        } catch (SQLException e) {
-            System.out.println("Listagem de alunos FALHOU!");
-            e.printStackTrace();
+
+        } catch (Exception ignored) {
+
         }
 
-        //Retornar todos os alunos encontrados no banco de dados.
         return alunos;
+
     }
 
-    // 1.1 - Consulta com filtro
+//        try (Connection conn = ConnectionFactory.getConnection()) {
+//            //Preparar consulta SQL.
+//            String sql = "SELECT * FROM aluno";
+//
+//            //Preparar statement com os parâmetros recebidos (nesta função não tem parâmetros, pois irá retornar todos os valores da tabela aluno)
+//            PreparedStatement stmt = conn.prepareStatement(sql);
+//
+//            //Executa consulta e armazena o retorno da consulta no objeto "rs".
+//            ResultSet rs = stmt.executeQuery();
+//
+//            //Criar um objeto aluno e guardar na lista de alunos.
+//            while(rs.next()){
+//                int id = rs.getInt("id");
+//                String nome = rs.getString("nome");
+//                int idade = rs.getInt("idade");
+//                String estado = rs.getString("estado");
+//
+//                alunos.add(new Aluno(
+//                        id,
+//                        nome,
+//                        idade,
+//                        estado
+//                ));
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Listagem de alunos FALHOU!");
+//            e.printStackTrace();
+//        }
+//
+//        //Retornar todos os alunos encontrados no banco de dados.
+//        return alunos;
+//    }
+
+    //    // 1.1 - Consulta com filtro
     public Aluno getById(int id) {
         //Preparar objeto aluno para receber os valores do banco de dados.
         Aluno aluno = new Aluno();
@@ -64,7 +92,7 @@ public class AlunoDAO {
             ResultSet rs = stmt.executeQuery();
 
             //Guardar valores retornados da tabela aluno no objeto aluno
-            if (rs.next()){
+            if (rs.next()) {
                 aluno.setId(rs.getInt("id"));
                 aluno.setNome(rs.getString("nome"));
                 aluno.setIdade(rs.getInt("idade"));
@@ -76,7 +104,7 @@ public class AlunoDAO {
             e.printStackTrace();
         }
 
-        //Retorna aluno encontrado no banco de dados.
+//    Retorna aluno encontrado no banco de dados.
         return aluno;
     }
 
@@ -89,9 +117,9 @@ public class AlunoDAO {
 
             //Preparar statement com os parâmetros recebidos
             PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1 , aluno.getNome());
+            stmt.setString(1, aluno.getNome());
             stmt.setInt(2, aluno.getIdade());
-            stmt.setString(3 , aluno.getEstado());
+            stmt.setString(3, aluno.getEstado());
 
             //Executa inserção e armazena o numero de linhas afetadas
             int rowsAffected = stmt.executeUpdate();
@@ -103,27 +131,28 @@ public class AlunoDAO {
         }
     }
 
+
     // 3 - Delete
-    public void delete(int id) {
-        try (Connection conn = ConnectionFactory.getConnection()) {
-
-            //Preparar SQL para deletar uma linha.
-            String sql = "DELETE FROM aluno WHERE id = ?";
-
-            //Preparar statement com os parâmetros recebidos
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setInt(1 , id);
-
-            //Executa delete e armazena o numero de linhas afetadas
-            int rowsAffected = stmt.executeUpdate();
-
-            System.out.println("Delete BEM SUCEDIDA! Foi deletada " + rowsAffected + " linha");
-        } catch (SQLException e) {
-            System.out.println("Delete FALHOU!");
-            e.printStackTrace();
-        }
-    }
-
+//    public void delete(int id) {
+//        try (Connection conn = ConnectionFactory.getConnection()) {
+//
+//            //Preparar SQL para deletar uma linha.
+//            String sql = "DELETE FROM aluno WHERE id = ?";
+//
+//            //Preparar statement com os parâmetros recebidos
+//            PreparedStatement stmt = conn.prepareStatement(sql);
+//            stmt.setInt(1 , id);
+//
+//            //Executa delete e armazena o numero de linhas afetadas
+//            int rowsAffected = stmt.executeUpdate();
+//
+//            System.out.println("Delete BEM SUCEDIDA! Foi deletada " + rowsAffected + " linha");
+//        } catch (SQLException e) {
+//            System.out.println("Delete FALHOU!");
+//            e.printStackTrace();
+//        }
+//    }
+//
     // 4 - Atualizar
     public void update(Aluno aluno) {
         try (Connection conn = ConnectionFactory.getConnection()) {
--- src/main/java/part3/QueriesExecution.java
+++ src/main/java/part3/QueriesExecution.java
@@ -11,38 +11,46 @@ public class QueriesExecution {
         AlunoDAO alunoDAO = new AlunoDAO();
 
         // =========================== 1 - Consulta =================================================
-        List<Aluno> alunos = alunoDAO.list();
+//        List<Aluno> alunos = alunoDAO.list();
 
-        alunos.stream().forEach(System.out::println);
-
-
-        // ======================= 1.1 - Consulta com filtro ========================================
-        Aluno alunoParaConsulta = alunoDAO.getById(1);
-
-        //System.out.println(alunoParaConsulta);
+//        alunos.forEach(System.out::println);
 
 
-        // =========================== 2 - Inserção =================================================
-        Aluno alunoParaInsercao = new Aluno(
-                "Matheus",
-                43,
-                "SP"
-        );
-
-        //alunoDAO.create(alunoParaInsercao);
-
-
+//        // ======================= 1.1 - Consulta com filtro ========================================
+//        Aluno alunoParaConsulta = alunoDAO.getById(3);
+//
+//        System.out.println(alunoParaConsulta);
+//
+//
+//        // =========================== 2 - Inserção =================================================
+//        Aluno alunoParaInsercao = new Aluno(
+//                "Matheus",
+//                43,
+//                "SP"
+//        );
+//
+//        alunoDAO.create(alunoParaInsercao);
+//
+//
         // =========================== 3 - Delete ===================================================
-        //alunoDAO.delete(1);
+//        alunoDAO.list().forEach(System.out::println);
+//
+//        alunoDAO.delete(4);
+//
+//        alunoDAO.list().forEach(System.out::println);
 
 
-        // =========================== 4 - Atualizar ================================================
+//        // =========================== 4 - Atualizar ================================================
+        alunoDAO.list().forEach(System.out::println);
+
         Aluno alunoParaAtualizar = alunoDAO.getById(3);
         alunoParaAtualizar.setNome("Joaquim");
         alunoParaAtualizar.setIdade(18);
         alunoParaAtualizar.setEstado("RS");
 
-        //alunoDAO.update(alunoParaAtualizar);
+        alunoDAO.update(alunoParaAtualizar);
+
+        alunoDAO.list().forEach(System.out::println);
     }
 
 }
